9ba2f9f9851e36fbc337df570e1a1f572cefa6a8
iegrsy <iegursoy28@gmail.com>
2017-08-13 21:06:00 +0300

    first commit

---
+++ app/src/main/java/com/example/ieg/mysqlconnectionapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.ieg.mysqlconnectionapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

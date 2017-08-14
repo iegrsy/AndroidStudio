5c46107d89ba69315cf6d121217f555f996258ed
iegrsy <iegursoy28@gmail.com>
2017-08-14 14:25:30 +0300

    first commit

--- app/src/main/java/com/example/ieg/mysqlconnectionapp/MainActivity.java
+++ app/src/main/java/com/example/ieg/mysqlconnectionapp/MainActivity.java
@@ -1,13 +1,59 @@
 package com.example.ieg.mysqlconnectionapp;
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
     public Button btn;
+    public TextView textView;
+    public Context context = MainActivity.this;
+    public String stringUrl = "http://ibrahimethem.tk/rehber.json";
+    public String jsonStr;
+    private static final String DEBUG_TAG = "HttpExample";
+
+    private String TAG = MainActivity.class.getSimpleName();
+    private ListView lv;
+
+    ArrayList<HashMap<String, String>> contactList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,13 +61,181 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btn = (Button) findViewById(R.id.button);
+        textView = (TextView) findViewById(R.id.textView);
+        lv = (ListView) findViewById(R.id.list);
+
+        contactList = new ArrayList<>();
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                btn.setText("hey");
+
+                // URL bilgisini UI'dan alıyoruz
+
+                ConnectivityManager connMgr = (ConnectivityManager)
+                        getSystemService(Context.CONNECTIVITY_SERVICE);
+                NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+                if (networkInfo != null && networkInfo.isConnected()) {
+
+                    new DownloadWebpageTask().execute(stringUrl);
+
+                } else {
+                    textView.setText("No network connection available.");
+                }
+
+                String name = btn.getText().equals("hey") ? "buton" : "hey";
+                btn.setText(name);
             }
         });
+
+
     }
 
+    private class DownloadWebpageTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+
+            // parametreler execute() çagırısından gelir: params[0] URL'nin yeridir.
+            try {
+                return downloadUrl(urls[0]);
+            } catch (IOException e) {
+                return "Web sayfası getirilemedi. Belki URL yanlıştır";
+            }
+        }
+        // onPostExecute AsyncTask'tan dönen cevabı gösterir.
+        @Override
+        protected void onPostExecute(String result) {
+            Log.w("msg",result);
+            textView.setText(result);
+            jsonStr = result;
+            new GetContacts().execute();
+        }
+    }
+
+    // verilen URL bağlantısı HttpURLConnection kullanarak sağlanır
+// web sayfası bağlantısındaki veri InputStream şeklinde gelir
+// InputStream String'e çevirilir.
+    private String downloadUrl(String myurl) throws IOException {
+        InputStream is = null;
+        // Web sayfasından gelen verinin sadece 500 karakterini göstermek için
+        int len = 500;
+
+        try {
+            URL url = new URL(myurl);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(10000 /* milliseconds */);
+            conn.setConnectTimeout(15000 /* milliseconds */);
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+            // Sorguyu başlatmak
+            conn.connect();
+            int response = conn.getResponseCode();
+            Log.d(DEBUG_TAG, "The response is: " + response);
+            is = conn.getInputStream();
+            // InputStream verisi string'e çeviriliyor
+            String contentAsString = readIt(is, len);
+            return contentAsString;
+
+            // InputStream'i uygulama işi tamamlandığında kaptamayı unutmayın
+        } finally {
+            if (is != null) {
+                is.close();
+            }
+        }
+    }
+    // InputStream'i okur ve String'e çevirir
+    public String readIt(InputStream stream, int len) throws IOException, UnsupportedEncodingException {
+        Reader reader = null;
+        reader = new InputStreamReader(stream, "UTF-8");
+        char[] buffer = new char[len];
+        reader.read(buffer);
+        return new String(buffer);
+    }
+
+    private class GetContacts extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            Toast.makeText(MainActivity.this,"Json Data is downloading",Toast.LENGTH_LONG).show();
+
+        }
+
+        @Override
+        protected Void doInBackground(Void... arg0) {
+
+
+            Log.e(TAG, "Response from url: " + jsonStr);
+            if (jsonStr != null) {
+                try {
+                    JSONObject jsonObj = new JSONObject(jsonStr);
+
+                    // Getting JSON Array node
+                    JSONArray contacts = jsonObj.getJSONArray("contacts");
+
+                    // looping through All Contacts
+                    for (int i = 0; i < contacts.length(); i++) {
+                        JSONObject c = contacts.getJSONObject(i);
+                        String id = c.getString("id");
+                        String name = c.getString("name");
+                        String email = c.getString("email");
+                        String address = c.getString("address");
+                        String gender = c.getString("gender");
+
+                        // Phone node is JSON Object
+                        JSONObject phone = c.getJSONObject("phone");
+                        String mobile = phone.getString("mobile");
+                        String home = phone.getString("home");
+                        String office = phone.getString("office");
+
+                        // tmp hash map for single contact
+                        HashMap<String, String> contact = new HashMap<>();
+
+                        // adding each child node to HashMap key => value
+                        contact.put("id", id);
+                        contact.put("name", name);
+                        contact.put("email", email);
+                        contact.put("mobile", mobile);
+
+                        // adding contact to contact list
+                        contactList.add(contact);
+                    }
+                } catch (final JSONException e) {
+                    Log.e(TAG, "Json parsing error: " + e.getMessage());
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getApplicationContext(),
+                                    "Json parsing error: " + e.getMessage(),
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    });
+
+                }
+
+            } else {
+                Log.e(TAG, "Couldn't get json from server.");
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(),
+                                "Couldn't get json from server. Check LogCat for possible errors!",
+                                Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            ListAdapter adapter = new SimpleAdapter(MainActivity.this, contactList,
+                    R.layout.list_item, new String[]{ "email","mobile"},
+                    new int[]{R.id.email, R.id.mobile});
+            lv.setAdapter(adapter);
+
+            Log.w("my app","buraya geldin mi");
+        }
+    }
 }
